fe47d39dfe8010804b4fabede4468a1b20379430
Michael Karbowiak <karbowiak@gmail.com>
2016-06-17 20:42:19 +0200

    Load all the new database models

--- src/Service/SystemServiceProvider.php
+++ src/Service/SystemServiceProvider.php
@@ -11,7 +11,22 @@ use Thessia\Lib\Config;
 use Thessia\Lib\Db;
 use Thessia\Lib\SessionHandler;
 use Thessia\Lib\Timer;
+use Thessia\Model\Database\blueprints;
+use Thessia\Model\Database\categoryIDs;
+use Thessia\Model\Database\certificates;
+use Thessia\Model\Database\constellations;
+use Thessia\Model\Database\graphicIDs;
+use Thessia\Model\Database\groupIDs;
+use Thessia\Model\Database\iconIDs;
 use Thessia\Model\Database\killmails;
+use Thessia\Model\Database\landmarks;
+use Thessia\Model\Database\regions;
+use Thessia\Model\Database\skinLicenses;
+use Thessia\Model\Database\skinMaterials;
+use Thessia\Model\Database\skins;
+use Thessia\Model\Database\solarSystems;
+use Thessia\Model\Database\tournamentRuleSets;
+use Thessia\Model\Database\typeIDs;
 
 class SystemServiceProvider extends AbstractServiceProvider
 {
@@ -76,6 +91,21 @@ class SystemServiceProvider extends AbstractServiceProvider
         $container->share("mongo", Client::class);
 
         // Models
-        //$container->share("killmails", killmails::class)->withArgument("config")->withArgument("mongo");
+        $container->share("blueprints", blueprints::class)->withArgument("config")->withArgument("mongo");
+        $container->share("categoryIDs", categoryIDs::class)->withArgument("config")->withArgument("mongo");
+        $container->share("certificates", certificates::class)->withArgument("config")->withArgument("mongo");
+        $container->share("constellations", constellations::class)->withArgument("config")->withArgument("mongo");
+        $container->share("graphicIDs", graphicIDs::class)->withArgument("config")->withArgument("mongo");
+        $container->share("groupIDs", groupIDs::class)->withArgument("config")->withArgument("mongo");
+        $container->share("iconIDs", iconIDs::class)->withArgument("config")->withArgument("mongo");
+        $container->share("killmails", killmails::class)->withArgument("config")->withArgument("mongo");
+        $container->share("landmarks", landmarks::class)->withArgument("config")->withArgument("mongo");
+        $container->share("regions", regions::class)->withArgument("config")->withArgument("mongo");
+        $container->share("skinLicenses", skinLicenses::class)->withArgument("config")->withArgument("mongo");
+        $container->share("skinMaterials", skinMaterials::class)->withArgument("config")->withArgument("mongo");
+        $container->share("skins", skins::class)->withArgument("config")->withArgument("mongo");
+        $container->share("solarSystems", solarSystems::class)->withArgument("config")->withArgument("mongo");
+        $container->share("tournamentRuleSets", tournamentRuleSets::class)->withArgument("config")->withArgument("mongo");
+        $container->share("typeIDs", typeIDs::class)->withArgument("config")->withArgument("mongo");
     }
 }
